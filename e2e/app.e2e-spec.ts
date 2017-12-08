6131d119daa14c7682117adb61a118ddc491fc50
René Winkler <rene.winkler@xovis.com>
2017-12-08 15:13:52 +0100

    add some tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { AppPage } from './app.po';
+import {browser} from "protractor";
 
 describe('client App', () => {
   let page: AppPage;
@@ -8,7 +9,29 @@ describe('client App', () => {
   });
 
   it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    page.navigateTo('/');
+    expect(page.getParagraphText()).toEqual('Welcome to cool cars app!');
   });
+
+  it('should be empty car list', () => {
+    page.navigateTo('/');
+    expect(page.getCarList().count()).toEqual(0);
+  });
+
+  it('should navigate to car-add component when add is clicked', () => {
+    page.navigateTo('/');
+    page.clickAddButton();
+    expect(browser.getCurrentUrl()).toContain(browser.baseUrl + '/car-add');
+  });
+
+  it('should enable or disable buttons', () => {
+    page.navigateTo('/car-add');
+    expect(page.getAddButtonInEditComponent().isEnabled()).toBe(false);
+    expect(page.getCancelButtonInEditComponent().isEnabled()).toBe(true);
+    expect(page.getDeleteButtonInEditComponent().isPresent()).toBe(false);
+    expect(page.getInputFieldButtonInEditComponent().isEnabled()).toBe(true);
+    page.setInputFieldText('any car');
+    expect(page.getAddButtonInEditComponent().isEnabled()).toBe(true);
+  });
+
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,11 +1,41 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
-  navigateTo() {
-    return browser.get('/');
+  navigateTo(path: string) {
+    return browser.get(path);
   }
 
   getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+    return element(by.css('mat-toolbar span')).getText();
   }
+
+  clickAddButton() {
+    return element(by.css('mat-card button')).click();
+  }
+
+  getCarList() {
+    return element.all(by.repeater('car in cars'));
+  }
+
+  getAddButtonInEditComponent() {
+    return element(by.id('save'));
+  }
+
+  getCancelButtonInEditComponent() {
+    return element(by.id('cancel'));
+  }
+
+  getDeleteButtonInEditComponent() {
+    return element(by.id('delete'));
+  }
+
+  getInputFieldButtonInEditComponent() {
+    return element(by.id('input'));
+  }
+
+  setInputFieldText(text) {
+    this.getInputFieldButtonInEditComponent().sendKeys(text);
+  }
+
+
 }
