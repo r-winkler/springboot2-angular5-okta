32435a4f533f79363b00e5df3a983174bb0b2f1e
René Winkler <rene.winkler@xovis.com>
2017-12-08 13:33:40 +0100

    inital commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,52 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {CarService} from "./shared/car/car.service";
+import { CarListComponent } from './car-list/car-list.component';
+import {HttpClientModule} from "@angular/common/http";
+import {MatButtonModule, MatCardModule, MatInputModule, MatListModule, MatToolbarModule} from "@angular/material";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {GiphyService} from "./shared/giphy/giphy.service";
+import { CarEditComponent } from './car-edit/car-edit.component';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/car-list', pathMatch: 'full' },
+  {
+    path: 'car-list',
+    component: CarListComponent
+  },
+  {
+    path: 'car-add',
+    component: CarEditComponent
+  },
+  {
+    path: 'car-edit/:id',
+    component: CarEditComponent
+  }
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarListComponent,
+    CarEditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatListModule,
+    MatToolbarModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [CarService, GiphyService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
